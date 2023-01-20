6ed69ba0bddbd4c42749c8590719323704047ac4
Matheus Rocha <matheusmartinsInsper>
2023-01-20 14:23:22 -0300

    concluindo o bakcend da app

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+//back-end API RESTful
+import Fastify from 'fastify'
+
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+
+app.listen({
+    port: 3333
+}).then(()=>{console.log('server running')})
+/* 
+metodo http: get, post, put, patch, delete
+*/ 
